fb6a486e9b18d0d18fb79d6642cc65100df56e76
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-02-08 18:07:47 +0800

    fix: FCC's modal is not showing

--- src/App.svelte
+++ src/App.svelte
@@ -5,14 +5,6 @@
   import { CONFIG } from "./settings.js";
 </script>
 
-<svelte:head>
-  <script
-    type="text/javascript"
-    src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
-    defer>
-  </script>
-</svelte:head>
-
 {#each Object.entries($CONFIG) as [name, length]}
   <LengthConfig ID_PREFIX={name} />
 {/each}
